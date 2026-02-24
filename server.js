e460d6c904ac8451b1faceb8a860ec61a550b480
edwinhuguito-dev <edwinhuguito@gmail.com>
2026-02-24 18:29:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ mongoose
   .then(() => console.log("Conectado a MongoDB Atlas"))
   .catch((err) => console.log("Error MongoDB:", err.message));
 
-// PUNTO 2: recibir parámetro por GET
-// Ejemplo: http://localhost:3000/api/servicio?dato=hola
+
 app.get("/api/servicio", (req, res) => {
   const dato = req.query.dato; // <-- parámetro GET
   if (!dato) {
@@ -20,6 +19,9 @@ app.get("/api/servicio", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
+app.get("/", (req, res) => {
+  res.send("PAFINAL API OK");
+});
 app.listen(PORT, () => {
   console.log("Servidor en puerto", PORT);
 });
